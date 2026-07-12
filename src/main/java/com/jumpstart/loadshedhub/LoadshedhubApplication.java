82e5322d444d5ac796b585d882f2a58d783d3f4c
koena08 <kayletsie@gmail.com>
2026-07-12 12:14:04 +0200

    chore: files

---
+++ src/main/java/com/jumpstart/loadshedhub/LoadshedhubApplication.java
@@ -0,0 +1,13 @@
+package com.jumpstart.loadshedhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadshedhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadshedhubApplication.class, args);
+	}
+
+}
